50aac6fe7d777b5fb65c67ec879ccbe1a0aad159
4ever2 <3417013+4ever2@users.noreply.github.com>
2024-09-10 15:02:03 +0200

    Simplify nix files

--- .nix/coq-overlays/elm-extraction/default.nix
+++ .nix/coq-overlays/elm-extraction/default.nix
@@ -1,5 +1,4 @@
-{ lib, mkCoqDerivation, which, coq
-  , metacoq, version ? null }:
+{ lib, mkCoqDerivation, which, coq, metacoq, version ? null }:
 
 with lib; mkCoqDerivation {
   pname = "elm-extraction";
@@ -9,25 +8,20 @@ with lib; mkCoqDerivation {
 
   inherit version;
   defaultVersion = with versions; switch [coq.coq-version metacoq.version] [
-    { cases = ["8.17" "1.3.1-8.17"]; out = "0.1.0"; }
-    { cases = ["8.18" "1.3.1-8.18"]; out = "0.1.0"; }
-    { cases = ["8.19" "1.3.1-8.19"]; out = "0.1.0"; }
+    { cases = [(range "8.17" "8.19") (range "1.3.1" "1.3.2")]; out = "0.1.0"; }
   ] null;
 
   release."0.1.0".sha256 = "EWjubBHsxAl2HuRAfJI3B9qzP2mj89eh0CUc8y7/7Ds=";
-  release."0.1.0".rev = "v0.1.0";
 
   releaseRev = v: "v${v}";
 
   propagatedBuildInputs = [ coq.ocamlPackages.findlib metacoq ];
 
-  patchPhase = ''patchShebangs ./tests/process-extraction-examples.sh'';
+  postPatch = ''patchShebangs ./tests/process-extraction-examples.sh'';
 
   meta = {
     description = "A framework for extracting Coq programs to Elm";
-    ## Kindly ask one of these people if they want to be an official maintainer.
-    ## (You might also consider adding yourself to the list of maintainers)
-    # maintainers = with maintainers; [ cohencyril siraben vbgl Zimmi48 ];
+    maintainers = with maintainers; [ _4ever2 ];
     license = licenses.mit;
   };
 }
